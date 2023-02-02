322c0fb60e0f8312c35e27f733e5d20a675c6a32
nick <n.iacurto@gmail.com>
2023-02-01 20:58:05 -0500

    Changed 10 to ten in multiple of ten file

--- Class_03/multiple_of_ten.py
+++ Class_03/multiple_of_ten.py
@@ -2,9 +2,9 @@ number = input("Give me a number ")
 
 if number.isnumeric():
     if int(number) % 10 == 0 and int(number) < 100:
-        print("Yes, the number is less than 100 and a multiple of 10.")
+        print("Yes, the number is less than 100 and a multiple of ten.")
     else:
-        print("No, the number is not less than 100 and a multiple of 10.")
+        print("No, the number is not less than 100 and a multiple of ten.")
 else:
     print("Error, you did not enter a number")
 
